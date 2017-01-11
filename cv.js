bdf29fdee713dcb1711bb3e2e414f61c8dad2585
Pirathaab <sritharan.pirathaab@gmail.com>
2017-01-11 14:59:42 +0100

    uptaded

--- cv.js
+++ cv.js
@@ -1,7 +1,7 @@
 $('.first.circle').circleProgress({
-  value: 0.8
+  value: 0.60
 }).on('circle-animation-progress', function(event, progress) {
-  $(this).find('strong').html(parseInt(80 * progress) + '<i>%</i>');
+  $(this).find('strong').html(parseInt(60 * progress) + '<i>%</i>');
 });
 
 $('.second.circle').circleProgress({
@@ -11,39 +11,39 @@ $('.second.circle').circleProgress({
 });
 
 $('.third.circle').circleProgress({
-  value: 0.4
+  value: 0.3
 }).on('circle-animation-progress', function(event, progress) {
-  $(this).find('strong').html(parseInt(40 * progress) + '<i>%</i>');
+  $(this).find('strong').html(parseInt(30 * progress) + '<i>%</i>');
 });
 
 $('.fourth.circle').circleProgress({
-  value: 0.2
-}).on('circle-animation-progress', function(event, progress) {
-  $(this).find('strong').html(parseInt(20 * progress) + '<i>%</i>');
-});
-
-$('.fifth.circle').circleProgress({
-  value: 0.1
-}).on('circle-animation-progress', function(event, progress) {
-  $(this).find('strong').html(parseInt(10 * progress) + '<i>%</i>');
-});
-
-$('.sixth.circle').circleProgress({
   value: 0.25
 }).on('circle-animation-progress', function(event, progress) {
   $(this).find('strong').html(parseInt(25 * progress) + '<i>%</i>');
 });
 
-$('.seventh.circle').circleProgress({
-  value: 1
+$('.fifth.circle').circleProgress({
+  value: 0.20
 }).on('circle-animation-progress', function(event, progress) {
-  $(this).find('strong').html(parseInt(100 * progress) + '<i>%</i>');
+  $(this).find('strong').html(parseInt(20 * progress) + '<i>%</i>');
+});
+
+$('.sixth.circle').circleProgress({
+  value: 0.30
+}).on('circle-animation-progress', function(event, progress) {
+  $(this).find('strong').html(parseInt(30 * progress) + '<i>%</i>');
+});
+
+$('.seventh.circle').circleProgress({
+  value: 0.95
+}).on('circle-animation-progress', function(event, progress) {
+  $(this).find('strong').html(parseInt(95 * progress) + '<i>%</i>');
 });
 
 $('.eight.circle').circleProgress({
-  value: 0.65
+  value: 0.60
 }).on('circle-animation-progress', function(event, progress) {
-  $(this).find('strong').html(parseInt(65 * progress) + '<i>%</i>');
+  $(this).find('strong').html(parseInt(60 * progress) + '<i>%</i>');
 });
 
 $('.nineth.circle').circleProgress({
